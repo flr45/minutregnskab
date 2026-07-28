334a32ac1f899815f75d6cda247660a48d56adc7
flr45 <151404060+flr45@users.noreply.github.com>
2026-07-28 11:45:56 +0200

    Create service-worker.js

---
+++ static/service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "tidsregistrering-v3";
+
+const URLS_TO_CACHE = [
+  "/",
+  "/static/manifest.json",
+  "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
